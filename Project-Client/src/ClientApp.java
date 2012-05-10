31015cd67aeeef490289a68f8109f66293f5d841
Alexandre <alesimar@gmail.com>
2012-05-10 14:42:02 -0400

    Object streams not working

--- Project-Client/src/ClientApp.java
+++ Project-Client/src/ClientApp.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -12,8 +14,9 @@ public class ClientApp
 	final private static int DEFAULT_PORT = 9999;
 	/**
 	 * @param args
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws ClassNotFoundException
 	{
 		//Game game1 = new Game();
 		//System.out.println("Welcome to Knight's Watch!!!");
@@ -22,20 +25,20 @@ public class ClientApp
 		//System.out.println(game1.getBoard().toString());
 		//game1.makeMoveForCurrentPlayer();
 		//}
-		
+
 		String host = args.length > 0 ? args[0] : DEFAULT_HOST;
 		int port = args.length > 1 ? Integer.parseInt(args[1]) :  DEFAULT_PORT;
-		
+
 		try
 		{
 			// connect to the server
 			Socket socket = new Socket(host, port);
-			
+
 			// create a scanner & printstream out of the socket's I/O streams
-			Scanner socketIn = new Scanner(socket.getInputStream());
-			PrintStream socketOut = new PrintStream(socket.getOutputStream());
-			
-			String playerNum = socketIn.nextLine();
+			ObjectInputStream socketIn = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream socketOut = new ObjectOutputStream(new PrintStream(socket.getOutputStream()));
+
+			String playerNum = (String) socketIn.readObject();
 			System.out.println("You are player number : " + playerNum);
 			// prepare STDIN for reading
 			Scanner stdin = new Scanner(System.in);
@@ -45,26 +48,26 @@ public class ClientApp
 			if (playerNum.equals("2"))
 			{
 				System.out.println("Waiting for player" + oppositePlayerNum + " to say something...");
-				echo = socketIn.nextLine();
+				echo = (String) socketIn.readObject();
 				// print to STDOUT
 				System.out.println(echo);
 			}
-			
+
 			while(!input.equals("quit"))
 			{
 				//Get the text
 				System.out.print("Say something: ");
 				input = stdin.nextLine();
 				// send the text
-				socketOut.println("Player number " + playerNum + " says: " + input);
-				
+				socketOut.writeObject("Player number " + playerNum + " says: " + input);
+
 				// receive the echo
 				System.out.println("Waiting for player" + oppositePlayerNum + " to say something...");
-				echo = socketIn.nextLine();
+				echo = (String) socketIn.readObject();
 
 				// print to STDOUT
 				System.out.println(echo);
-				
+
 			}
 		}
 		catch (UnknownHostException e)
